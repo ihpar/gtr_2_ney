4bf57f04bc0950ff4ea419733e9b3d291d54caf2
ihpar <ismail.hakki.parlak@gmail.com>
2025-06-04 13:01:35 +0300

    cleanup pt 2

--- train_cgan.py
+++ train_cgan.py
@@ -72,7 +72,8 @@ def train_cgan(device,
             running_disc += d_loss.item()
             num_batches += 1
             if num_batches % 200 == 0:
-                print(f" - B: {num_batches}\t D: {d_loss.item():.6f}\t G: {g_loss.item():.6f}\t Gadv: {g_adv_loss.item():.6f}")
+                print(
+                    f" - B: {num_batches}\t D: {d_loss.item():.6f}\t G: {g_loss.item():.6f}\t Gadv: {g_adv_loss.item():.6f}")
 
         running_gen /= num_batches
         running_disc /= num_batches
@@ -83,41 +84,3 @@ def train_cgan(device,
 
     print("Training Complete!")
     return generator, history
-
-
-if __name__ == "__main__":
-    import pickle
-
-    import torch
-    import torch.nn as nn
-    import torch.optim as optim
-    from models.unet_gen import UNetGenerator
-    from models.patch_gan_discriminator import PatchGANDiscriminator
-
-    from dataset import build_data_loaders
-
-    with open("dataset/features/min_max.pkl", "rb") as handle:
-        min_max = pickle.load(handle)
-
-    train_data_loader, _ = build_data_loaders(
-        min_max, part="magnitude", test_size=0.1)
-
-    # Models
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    generator = UNetGenerator(in_channels=1, out_channels=1).to(device)
-    discriminator = PatchGANDiscriminator(in_channels=2).to(device)
-
-    # Loss Functions
-    adversarial_loss = nn.BCELoss()
-    l1_loss = nn.L1Loss()
-
-    # Optimizers
-    lr = 2e-4
-    optimizer_G = optim.Adam(generator.parameters(), lr=lr, betas=(0.5, 0.999))
-    optimizer_D = optim.Adam(discriminator.parameters(),
-                             lr=lr, betas=(0.5, 0.999))
-
-    # Train the Model
-    train_cgan(device, train_data_loader, generator,
-               discriminator, adversarial_loss, l1_loss,
-               optimizer_G, optimizer_D, 1)
